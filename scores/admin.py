eaa05e9f0140c877c53d8443f0ab0c70e1b322c0
Samir Fors <samir.fors@gmail.com>
2012-04-04 13:16:35 +0200

    Finished building data model for app.

---
+++ scores/admin.py
@@ -0,0 +1,4 @@
+from scores.models import Player
+from django.contrib import admin
+
+admin.site.register(Player)
--- scores/models.py
+++ scores/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
+class Tournament(models.Model):
+	date_played = models.DateTimeField('date played')
+	def __unicode__(self):
+		return self.date_played
+
 class Player(models.Model):
 	name = models.CharField(max_length=200)
+	def __unicode__ (self):
+		return self.name
 
 class Fixture(models.Model):
-	date_played = models.DateTimeField('date played')
+	tournament = models.ForeignKey(Tournament)
 	player1 = models.ForeignKey(Player, related_name='player1')
 	player2 = models.ForeignKey(Player, related_name='player2')
+	game = 'player1 - player2'
+	def __unicode__(self):
+		return self.game
+
+class Winner(models.Model):
+	fixture = models.ForeignKey(Fixture)
+	winner = models.ForeignKey(Player)
+	fpoints = models.IntegerField('f-points')
+	def __unicode__(self):
+		return self.winner
